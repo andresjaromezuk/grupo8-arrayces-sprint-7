4b82b04310349d78f8b26e6db29cbc2db3989050
andresjaromezuk <andres.jaromezuk@gmail.com>
2022-10-23 11:57:13 -0300

    modificación burger menu

--- public/js/header.js
+++ public/js/header.js
@@ -2,11 +2,22 @@ window.onload = function(){
 
     const burgerMenu = document.querySelector('.burgerMenu')
     const burgerNavI = document.querySelector('.burgerNavI')
-    console.log(burgerMenu)
-    console.log(burgerNavI)
+
+    
+    
     burgerMenu.addEventListener('click', function(){
+        
         burgerNavI.classList.toggle('active')
-       
+
+        if(burgerNavI.classList.contains('active')){
+            burgerMenu.innerHTML= '<span class="material-symbols-outlined"> close </span>'
+        }else{
+            burgerMenu.innerHTML= '<span class="material-symbols-outlined">menu</span>'
+        }
+
     })
+        
+
+
 
 }
